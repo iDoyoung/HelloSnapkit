0b4d9ad26c9601c7f8a2e77ba3b8b6ea5a958f01
iDoyoung <ido.young@icloud.com>
2021-08-27 12:12:31 +0900

    Make Collection View Programmatically

--- HelloSnapKit/ViewController.swift
+++ HelloSnapKit/ViewController.swift
@@ -10,29 +10,47 @@ import SnapKit
 
 class ViewController: UIViewController {
 
-    lazy var sampleTableView: UITableView = {
-        let tableView = UITableView()
-        
-        return tableView
+    var colors = [#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1), #colorLiteral(red: 0.1764705926, green: 0.01176470611, blue: 0.5607843399, alpha: 1), #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1), #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1), #colorLiteral(red: 0.1764705926, green: 0.01176470611, blue: 0.5607843399, alpha: 1), #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1), #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1), #colorLiteral(red: 0.1411764771, green: 0.3960784376, blue: 0.5647059083, alpha: 1), #colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1), #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)]
+    
+    private let sectionInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+//    lazy var sampleTableView: UITableView = {
+//        let tableView = UITableView()
+//
+//        return tableView
+//    }()
+    
+    private lazy var sampleCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.itemSize = CGSize(width: 120, height: 120)
+        let collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .brown
+        collectionView.register(CustomCell.self, forCellWithReuseIdentifier: "cell")
+        return collectionView
     }()
     
-    lazy var sampleCollectionView: UICollectionView = {
-        let collectionView = UICollectionView()
-        
-        return collectionView
+    private lazy var sampleView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.systemPink
+        return view
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.sampleCollectionView.delegate = self
-        self.sampleCollectionView.dataSource = self
-        self.view.addSubview(sampleTableView)
-        sampleTableView.snp.makeConstraints { make in
+        self.view.addSubview(sampleCollectionView)
+        sampleCollectionView.snp.makeConstraints { make in
             make.top.equalTo(self.view.safeArea.top)
             make.bottom.equalTo(self.view.safeArea.bottom)
             make.width.equalTo(self.view)
         }
-        print("Testing commit in XCode")
+//        self.view.addSubview(sampleView)
+//        sampleView.snp.makeConstraints { make in
+//            make.top.equalTo(self.view.safeArea.top)
+//            make.bottom.equalTo(self.view.safeArea.bottom)
+//            make.width.equalTo(self.view)
+//        }
+        self.sampleCollectionView.delegate = self
+        self.sampleCollectionView.dataSource = self
     }
 
 
@@ -44,18 +62,51 @@ extension UIView {
     }
 }
 
-extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
+        return colors.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? CustomCell else {
+            return UICollectionViewCell()
+        }
+        cell.colorView.backgroundColor = colors[indexPath.item]
+        return cell
     }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.frame.width
+        let widthPadding = sectionInsets.left * 4
+        let cellWidth = (width - widthPadding)/3
+        
+        return CGSize(width: cellWidth, height: cellWidth)
+    }
     
 }
 
-class CollectionCell: UICollectionViewCell {
+class CustomCell: UICollectionViewCell {
     
+    let colorView: UIView = {
+        let colorView = UIView()
+        
+        return colorView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        contentView.addSubview(colorView)
+        colorView.snp.makeConstraints { make in
+            make.top.equalTo(self.contentView)
+            make.bottom.equalTo(self.contentView)
+            make.left.equalTo(self.contentView.snp.left)
+            make.right.equalTo(self.contentView.snp.right)
+        }
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
